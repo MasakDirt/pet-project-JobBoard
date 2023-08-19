6c5a1ecdea133e5eb0a9d3f22ff7789853c8915c
maksym_korniev <maksimkarulet8@gmail.com>
2023-08-19 18:56:20 +0300

    adding all project files

---
+++ src/main/java/com/board/job/JobBoardApplication.java
@@ -0,0 +1,11 @@
+package com.board.job;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobBoardApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JobBoardApplication.class, args);
+    }
+}
