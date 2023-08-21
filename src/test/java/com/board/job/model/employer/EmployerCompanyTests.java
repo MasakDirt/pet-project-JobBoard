18fb01fdbd8ef77f66aa89ad3fd0a79ea707648a
maksym_korniev <maksimkarulet8@gmail.com>
2023-08-21 18:37:13 +0300

    added new EmployerCompanyTests class

---
+++ src/test/java/com/board/job/model/employer/EmployerCompanyTests.java
@@ -0,0 +1,26 @@
+package com.board.job.model.employer;
+
+import com.board.job.model.ValidationHelper;
+import com.board.job.model.entity.employer.EmployerCompany;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class EmployerCompanyTests {
+    private static EmployerCompany employerCompany;
+
+    @BeforeAll
+    public static void init() {
+        employerCompany = new EmployerCompany();
+        employerCompany.setId(1L);
+        employerCompany.setAboutCompany("This is company...");
+        employerCompany.setWebSite("company.dot.ua");
+    }
+
+    @Test
+    public void test_Valid_EmployerCompany() {
+        Assertions.assertEquals(0, ValidationHelper.getViolation(employerCompany).size());
+    }
+}
