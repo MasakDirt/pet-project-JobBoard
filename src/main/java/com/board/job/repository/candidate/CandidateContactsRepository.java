6603455a9b12438984f9c552eff2cca063ef13df
maksym_korniev <maksimkarulet8@gmail.com>
2023-08-22 22:38:42 +0300

    CandidateContactsRepository added method for get Candidate with property picture attached by id

--- src/main/java/com/board/job/repository/candidate/CandidateContactsRepository.java
+++ src/main/java/com/board/job/repository/candidate/CandidateContactsRepository.java
@@ -2,6 +2,10 @@ package com.board.job.repository.candidate;
 
 import com.board.job.model.entity.candidate.CandidateContacts;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface CandidateContactsRepository extends JpaRepository<CandidateContacts, Long> {
+    @Query("SELECT c FROM CandidateContacts c LEFT JOIN FETCH c.profilePicture WHERE c.id = :id")
+    CandidateContacts findWithPropertyPictureAttachedById(@Param("id") long id);
 }
