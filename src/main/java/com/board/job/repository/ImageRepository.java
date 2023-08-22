b7b2abe76313dd55754b6a0b64ca217fa7b8bef2
maksym_korniev <maksimkarulet8@gmail.com>
2023-08-22 22:34:53 +0300

    delete image entities

--- src/main/java/com/board/job/repository/ImageRepository.java
+++
@@ -1,9 +0,0 @@
-package com.board.job.repository;
-
-import com.board.job.model.entity.Image;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ImageRepository extends JpaRepository<Image, Long> {
-}
