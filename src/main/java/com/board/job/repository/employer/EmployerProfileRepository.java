85341fda360daaea18794f75112945d91237cc7b
maksym_korniev <maksimkarulet8@gmail.com>
2023-08-23 18:12:07 +0300

    EmployerProfileRepository added new method findWithPropertyPictureAttachedById

--- src/main/java/com/board/job/repository/employer/EmployerProfileRepository.java
+++ src/main/java/com/board/job/repository/employer/EmployerProfileRepository.java
@@ -2,8 +2,14 @@ package com.board.job.repository.employer;
 
 import com.board.job.model.entity.employer.EmployerProfile;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface EmployerProfileRepository extends JpaRepository<EmployerProfile, Long> {
+    @Query("SELECT e FROM EmployerProfile e LEFT JOIN FETCH e.profilePicture WHERE e.id = :id")
+    Optional<EmployerProfile> findWithPropertyPictureAttachedById(@Param("id") long id);
 }
