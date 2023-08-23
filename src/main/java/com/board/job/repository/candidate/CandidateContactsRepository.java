dcc249f7b2edfaeca520d3c8a3ee0e0db94fdbc2
maksym_korniev <maksimkarulet8@gmail.com>
2023-08-23 18:12:40 +0300

    CandidateContactsRepository reformat method findWithPropertyPictureAttachedById it`s return optional now

--- src/main/java/com/board/job/repository/candidate/CandidateContactsRepository.java
+++ src/main/java/com/board/job/repository/candidate/CandidateContactsRepository.java
@@ -5,7 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.Optional;
+
 public interface CandidateContactsRepository extends JpaRepository<CandidateContacts, Long> {
     @Query("SELECT c FROM CandidateContacts c LEFT JOIN FETCH c.profilePicture WHERE c.id = :id")
-    CandidateContacts findWithPropertyPictureAttachedById(@Param("id") long id);
+    Optional<CandidateContacts> findWithPropertyPictureAttachedById(@Param("id") long id);
 }
