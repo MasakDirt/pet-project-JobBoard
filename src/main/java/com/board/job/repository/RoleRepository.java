9469955848bbcae41951f64006be9c86faa414e5
maksym_korniev <maksimkarulet8@gmail.com>
2023-08-31 03:17:40 +0300

    RoleRepository class added new method for getting roles by user id

--- src/main/java/com/board/job/repository/RoleRepository.java
+++ src/main/java/com/board/job/repository/RoleRepository.java
@@ -4,9 +4,12 @@ import com.board.job.model.entity.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
     Optional<Role> findByName(String name);
+
+    List<Role> findAllByUsersId(long id);
 }
