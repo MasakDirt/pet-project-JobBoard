7e7eb172b21ec53078a5b0b7633097ecf47997d6
maksym_korniev <maksimkarulet8@gmail.com>
2023-08-31 14:04:30 +0300

    added new AuthEntryPointJwt class for handling not authorized exceptions

---
+++ src/main/java/com/board/job/component/AuthEntryPointJwt.java
@@ -0,0 +1,22 @@
+package com.board.job.component;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Slf4j
+@Component
+public class AuthEntryPointJwt implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+        log.info("Content-Type: {}", request.getContentType());
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Error: Unauthorized (please authorize before going to this URL");
+    }
+}
