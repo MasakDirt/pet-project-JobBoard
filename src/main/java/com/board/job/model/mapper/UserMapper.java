3f082b7052c5d1afaf18350dc747fcbe8457c099
maksym_korniev <maksimkarulet8@gmail.com>
2023-09-01 17:23:57 +0300

    UserMapper added methods for getting user from UserUpdateRequestWithPassword and UserUpdateRequest

--- src/main/java/com/board/job/model/mapper/UserMapper.java
+++ src/main/java/com/board/job/model/mapper/UserMapper.java
@@ -2,11 +2,19 @@ package com.board.job.model.mapper;
 
 import com.board.job.model.dto.user.UserCreateRequest;
 import com.board.job.model.dto.user.UserResponse;
+import com.board.job.model.dto.user.UserUpdateRequest;
+import com.board.job.model.dto.user.UserUpdateRequestWithPassword;
 import com.board.job.model.entity.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
     User getUserFromUserCreate(UserCreateRequest userCreateRequest);
     UserResponse getUserResponseFromUser(User user);
+
+    @Mapping(target = "password", expression = "java(userUpdateRequest.getNewPassword())")
+    User getUserFromUserUpdateRequestPass(UserUpdateRequestWithPassword userUpdateRequest);
+
+    User getUserFromUserUpdateRequest(UserUpdateRequest userUpdateRequest);
 }
