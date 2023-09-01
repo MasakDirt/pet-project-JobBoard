73b5e3010d0203217313ce119917d4322e55498e
maksym_korniev <maksimkarulet8@gmail.com>
2023-09-01 17:26:57 +0300

    created new UserAuthService for checking user authorizations

---
+++ src/main/java/com/board/job/service/authorization/UserAuthService.java
@@ -0,0 +1,34 @@
+package com.board.job.service.authorization;
+
+import com.board.job.model.entity.User;
+import com.board.job.service.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class UserAuthService {
+    private final UserService userService;
+
+    public boolean isUsersSame(long id, String email) {
+        return getUser(email).getId() == id;
+    }
+
+    public boolean isUserAdminOrUsersSameById(long id, String email) {
+        return isAdmin(email) || isUsersSame(id, email);
+    }
+
+    public boolean isUserAdminOrUsersSameByEmail(String email, String authEmail) {
+        return isAdmin(authEmail) || getUser(authEmail).getEmail().equals(email);
+    }
+
+    private boolean isAdmin(String email) {
+       return getUser(email).getRoles()
+                .stream()
+                .anyMatch(role -> role.getName().equals("ADMIN"));
+    }
+
+    private User getUser(String email) {
+        return userService.readByEmail(email);
+    }
+}
