8515ce1528bf5b4a40fc2674c1a636b056bd64c4
maksym_korniev <maksimkarulet8@gmail.com>
2023-09-02 23:15:44 +0300

    CandidateContactService delete from method create params pdfFilename and photoFilename

--- src/main/java/com/board/job/service/candidate/CandidateContactService.java
+++ src/main/java/com/board/job/service/candidate/CandidateContactService.java
@@ -2,32 +2,18 @@ package com.board.job.service.candidate;
 
 import com.board.job.model.entity.candidate.CandidateContact;
 import com.board.job.repository.candidate.CandidateContactRepository;
-import com.board.job.service.PDFService;
 import com.board.job.service.UserService;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-
 @Service
 @AllArgsConstructor
 public class CandidateContactService {
     private final UserService userService;
-    private final PDFService pdfService;
     private final CandidateContactRepository candidateContactRepository;
 
-    public CandidateContact create(String pdfFilename, String photoFilename, long ownerId, CandidateContact candidateContact) {
-        if (pdfFilename != null) {
-            candidateContact.setPdf(pdfService.create(pdfFilename));
-        }
-
-        if (photoFilename != null) {
-            candidateContact.setProfilePicture(setImageContent(photoFilename));
-        }
-
+    public CandidateContact create(long ownerId, CandidateContact candidateContact) {
         candidateContact.setOwnerWithFields(userService.readById(ownerId));
         return candidateContactRepository.save(candidateContact);
     }
@@ -45,25 +31,5 @@ public class CandidateContactService {
 
     public void delete(long id) {
         candidateContactRepository.delete(readById(id));
-    }
-
-    public byte[] getWithPropertyPictureAttachedById(long id) {
-        byte[] profilePicture = candidateContactRepository.findWithPropertyPictureAttachedById(id);
-        return profilePicture == null ? new byte[]{} : profilePicture;
-    }
-
-    public void saveProfilePicture(long id, byte[] imageBytes) {
-        var candidateContacts = readById(id);
-        candidateContacts.setProfilePicture(imageBytes);
-
-        candidateContactRepository.save(candidateContacts);
-    }
-
-    private byte[] setImageContent(String filename) {
-        try {
-            return Files.readAllBytes(Path.of(filename));
-        } catch (IOException io) {
-            throw new IllegalArgumentException(String.format("File with name %s not found", filename));
-        }
     }
 }
