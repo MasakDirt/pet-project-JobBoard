dd2c305e8dbacdef8a90f67b2aa5dee6e6811f7c
maksym_korniev <maksimkarulet8@gmail.com>
2023-09-03 23:36:09 +0300

    added new class CandidateContactMapper for mapping candidateContact to CandidateContactResponse and CandidateContactRequest to CandidateContact

---
+++ src/main/java/com/board/job/model/mapper/CandidateContactMapper.java
@@ -0,0 +1,15 @@
+package com.board.job.model.mapper;
+
+import com.board.job.model.dto.candidate_contact.CandidateContactRequest;
+import com.board.job.model.dto.candidate_contact.CandidateContactResponse;
+import com.board.job.model.entity.candidate.CandidateContact;
+import org.mapstruct.Mapper;
+
+
+
+@Mapper(componentModel = "spring")
+public interface CandidateContactMapper {
+    CandidateContactResponse getCandidateContactResponseFromCandidateContact(CandidateContact contact);
+
+    CandidateContact getCandidateContactFromCandidateContactRequest(CandidateContactRequest request);
+}
