6886604b357ed5a2188afa01824f6f1000e5c582
maksym_korniev <maksimkarulet8@gmail.com>
2023-09-05 00:55:28 +0300

    created new authorization service class for checking valid auth users

---
+++ src/main/java/com/board/job/service/authorization/AuthCandidateProfileService.java
@@ -0,0 +1,21 @@
+package com.board.job.service.authorization;
+
+import com.board.job.model.entity.candidate.CandidateProfile;
+import com.board.job.service.candidate.CandidateProfileService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class AuthCandidateProfileService {
+    private final UserAuthService userAuthService;
+    private final CandidateProfileService candidateProfileService;
+
+    public boolean isUsersSameByIdAndUserOwnerCandidateProfile(long userId, long candidateId, String authEmail) {
+        return userAuthService.isUsersSame(userId, authEmail) && getProfile(candidateId).getOwner().getId() == userId;
+    }
+
+    public CandidateProfile getProfile(long id) {
+        return candidateProfileService.readById(id);
+    }
+}
