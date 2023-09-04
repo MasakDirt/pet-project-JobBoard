cdb9d8b5aee140f0d7634725e9f35146ccdb1a24
maksym_korniev <maksimkarulet8@gmail.com>
2023-09-05 00:55:01 +0300

    CandidateProfileMapper added methods to map CutCandidateProfileResponse and CandidateProfile

--- src/main/java/com/board/job/model/mapper/candidate/CandidateProfileMapper.java
+++ src/main/java/com/board/job/model/mapper/candidate/CandidateProfileMapper.java
@@ -1,10 +1,16 @@
 package com.board.job.model.mapper.candidate;
 
-import com.board.job.model.dto.candidate_profile.CandidateProfileResponse;
+import com.board.job.model.dto.candidate_profile.CandidateProfileRequest;
+import com.board.job.model.dto.candidate_profile.CutCandidateProfileResponse;
+import com.board.job.model.dto.candidate_profile.FullCandidateProfileResponse;
 import com.board.job.model.entity.candidate.CandidateProfile;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface CandidateProfileMapper {
-    CandidateProfileResponse getCandidateProfileResponseFromCandidateProfile(CandidateProfile candidateProfile);
+    FullCandidateProfileResponse getCandidateProfileResponseFromCandidateProfile(CandidateProfile candidateProfile);
+
+    CutCandidateProfileResponse getCutCandidateProfileResponseFromCandidateProfile(CandidateProfile candidateProfile);
+
+    CandidateProfile getCandidateProfileFromCandidateProfileRequest(CandidateProfileRequest request);
 }
