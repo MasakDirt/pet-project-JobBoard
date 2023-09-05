ccdc3a658a534f609b5600c308f73b2987d5b8ca
maksym_korniev <maksimkarulet8@gmail.com>
2023-09-05 14:47:45 +0300

    EmployerProfileRequest created new class request for creating or updating one

---
+++ src/main/java/com/board/job/model/dto/employer_profile/EmployerProfileRequest.java
@@ -0,0 +1,40 @@
+package com.board.job.model.dto.employer_profile;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+
+@Data
+@Getter
+@Builder
+public class EmployerProfileRequest {
+
+    @JsonProperty(value = "employer_name")
+    @NotBlank(message = "The employer name cannot be blank")
+    private String employerName;
+
+    @JsonProperty(value = "position")
+    @NotBlank(message = "You must write your position")
+    private String positionInCompany;
+
+    @JsonProperty(value = "company")
+    @NotBlank(message = "You should write your company name")
+    private String companyName;
+
+    @NotNull
+    @Pattern(regexp = "^@.*", message = "The telegram account must started with '@' character")
+    private String telegram;
+
+    @NotBlank(message = "You must write valid phone number.")
+    private String phone;
+
+    @NotNull
+    @JsonProperty(value = "linked_in")
+    @Pattern(regexp = "^www\\.linkedin\\.co.*$",
+            message = "Must be a valid LinkedIn link!")
+    private String linkedInProfile;
+}
