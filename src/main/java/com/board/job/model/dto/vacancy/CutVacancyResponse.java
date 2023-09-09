f652e19e5d6f8607782191bd0687c1c646a1b3b1
maksym_korniev <maksimkarulet8@gmail.com>
2023-09-09 21:23:41 +0300

    CutVacancyResponse class added postedAt field

--- src/main/java/com/board/job/model/dto/vacancy/CutVacancyResponse.java
+++ src/main/java/com/board/job/model/dto/vacancy/CutVacancyResponse.java
@@ -2,7 +2,10 @@ package com.board.job.model.dto.vacancy;
 
 import com.board.job.model.entity.sample.Category;
 import com.board.job.model.entity.sample.JobDomain;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.validation.constraints.Min;
@@ -11,6 +14,8 @@ import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Data;
 import lombok.Getter;
+
+import java.time.LocalDateTime;
 
 @Data
 @Getter
@@ -39,4 +44,9 @@ public class CutVacancyResponse {
     @JsonProperty(value = "work_experience")
     @Min(value = 0, message = "The work experience must be at least 0 years.")
     private double workExperience;
+
+    @JsonProperty("posted_at")
+    @JsonDeserialize(using = LocalDateDeserializer.class)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime postedAt;
 }
