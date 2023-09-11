68e6bcc53e4637921597889a84d03da555fe3694
maksym_korniev <maksimkarulet8@gmail.com>
2023-09-11 22:39:13 +0300

    AuthVacancyService added new method isUsersSameByIdAndUserOwnerEmployerProfileAndEmployerProfileOwnerOfVacancy and getUser

--- src/main/java/com/board/job/service/authorization/AuthVacancyService.java
+++ src/main/java/com/board/job/service/authorization/AuthVacancyService.java
@@ -1,6 +1,8 @@
 package com.board.job.service.authorization;
 
+import com.board.job.model.entity.User;
 import com.board.job.model.entity.Vacancy;
+import com.board.job.service.UserService;
 import com.board.job.service.VacancyService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -8,6 +10,7 @@ import org.springframework.stereotype.Service;
 @Service
 @AllArgsConstructor
 public class AuthVacancyService {
+    private final UserService userService;
     private final UserAuthService userAuthService;
     private final VacancyService vacancyService;
 
@@ -18,6 +21,13 @@ public class AuthVacancyService {
                 && getVacancy(id).getEmployerProfile().getId() == employerId;
     }
 
+    public boolean isUsersSameByIdAndUserOwnerEmployerProfileAndEmployerProfileOwnerOfVacancy(
+            long ownerId, long employerId, long id) {
+
+        return getUser(ownerId).getEmployerProfile().getId() == employerId
+                && getVacancy(id).getEmployerProfile().getId() == employerId;
+    }
+
     public boolean isUsersSameAndEmployerProfileOwnerOfVacancy(long ownerId, long employerId, long id, String authEmail) {
         return userAuthService.isUsersSame(ownerId, authEmail)
                 && getVacancy(id).getEmployerProfile().getId() == employerId;
@@ -25,5 +35,9 @@ public class AuthVacancyService {
 
     public Vacancy getVacancy(long id) {
         return vacancyService.readById(id);
+    }
+
+    private User getUser(long id) {
+        return userService.readById(id);
     }
 }
