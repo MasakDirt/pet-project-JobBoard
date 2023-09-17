fea515fb0f9a909c437196b0530eeeda6524b532
maksym_korniev <maksimkarulet8@gmail.com>
2023-09-18 00:50:15 +0300

    VacancyServiceMockitoTests refactor testing update method and created 2 new tests for methods - getSorted and getAllByEmployerProfileId

--- src/test/java/com/board/job/service/mockito/VacancyServiceMockitoTests.java
+++ src/test/java/com/board/job/service/mockito/VacancyServiceMockitoTests.java
@@ -7,13 +7,17 @@ import com.board.job.repository.VacancyRepository;
 import com.board.job.service.UserService;
 import com.board.job.service.VacancyService;
 import org.apache.cassandra.utils.Pair;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.mockito.Mockito.*;
@@ -49,7 +53,7 @@ public class VacancyServiceMockitoTests {
     @Test
     public void test_Update() {
         when(vacancyRepository.findById(0L)).thenReturn(Optional.of(new Vacancy()));
-        vacancyService.update(new Vacancy());
+        vacancyService.update(0L, new Vacancy());
 
         verify(vacancyRepository, times(1)).save(new Vacancy());
     }
@@ -67,5 +71,24 @@ public class VacancyServiceMockitoTests {
         vacancyService.getAll();
 
         verify(vacancyRepository, times(1)).findAll();
+    }
+
+    @Test
+    public void test_getSorted() {
+        when(vacancyRepository.findAll(Pageable.ofSize(2))).thenReturn(Page.empty());
+        vacancyService.getSorted(Pageable.ofSize(2));
+
+        verify(vacancyRepository, times(1)).findAll(Pageable.ofSize(2));
+    }
+
+    @Test
+    public void test_getAllByEmployerProfileId() {
+        long profileId = 3L;
+        List<Vacancy> expected = List.of(new Vacancy(), new Vacancy());
+        when(vacancyRepository.getVacanciesByEmployerProfileId(profileId)).thenReturn(expected);
+        List<Vacancy> actual = vacancyService.getAllByEmployerProfileId(profileId);
+
+        verify(vacancyRepository, times(1)).getVacanciesByEmployerProfileId(profileId);
+        Assertions.assertEquals(expected, actual);
     }
 }
