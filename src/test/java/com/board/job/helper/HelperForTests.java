4297f4f99b043934ad12402326c6cb79f15e65b2
maksym_korniev <maksimkarulet8@gmail.com>
2023-09-19 00:40:47 +0300

    HelperForTests added new method for getting user token by it`s credentials

--- src/test/java/com/board/job/helper/HelperForTests.java
+++ src/test/java/com/board/job/helper/HelperForTests.java
@@ -1,13 +1,18 @@
 package com.board.job.helper;
 
+import com.board.job.model.dto.login.LoginRequest;
 import com.board.job.model.dto.user.UserCreateRequest;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import jakarta.validation.ConstraintViolation;
 import jakarta.validation.Validation;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.Set;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 public class HelperForTests {
     public static <T> Set<ConstraintViolation<T>> getViolation(T testedClass) {
@@ -27,5 +32,17 @@ public class HelperForTests {
 
     public static UserCreateRequest createUser(String firstName, String lastName, String email, String password) {
         return new UserCreateRequest(firstName, lastName, email, password);
+    }
+
+    public static String getLoginToken(MockMvc mvc, String url, String email, String password) throws Exception {
+        return mvc.perform(post(url)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(
+                                asJsonString(LoginRequest.of(email, password))
+                        )
+                )
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
     }
 }
