06d4e5d7601f73312972bc9142f94edb16918ade
maksym_korniev <maksimkarulet8@gmail.com>
2023-10-17 00:49:34 +0300

    Category enum added toString realization

--- src/main/java/com/board/job/model/entity/sample/Category.java
+++ src/main/java/com/board/job/model/entity/sample/Category.java
@@ -2,5 +2,21 @@ package com.board.job.model.entity.sample;
 
 public enum Category {
     JAVA, PYTHON, JAVASCRIPT, C_PLUS_PLUS, C_SHARP, PHP, RUBY, SWIFT, GO, TYPESCRIPT, KOTLIN, HTML, CSS, SQL,
-    RUST, MATLAB, PERL, SHELL, OBJECTIVE_C, QA
+    RUST, MATLAB, PERL, SHELL, OBJECTIVE_C, QA;
+
+    @Override
+    public String toString() {
+        return switch (name()) {
+            case "QA" -> "QA";
+            case "PHP" -> "PHP";
+            case "CSS" -> "CSS";
+            case "SQL" -> "SQL";
+            case "C_PLUS_PLUS" -> "C++";
+            case "C_SHARP" -> "C#";
+            case "JAVASCRIPT" -> "JavaScript";
+            case "TYPESCRIPT" -> "TypeScript";
+            case "OBJECTIVE_C" -> "Objective-C";
+            default -> name().charAt(0) + name().substring(1).toLowerCase();
+        };
+    }
 }
