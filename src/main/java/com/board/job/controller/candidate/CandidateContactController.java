afbd8e0d73ed67ecb4835955ccaa98651dc0bd8a
maksym_korniev <maksimkarulet8@gmail.com>
2023-10-19 00:55:46 +0300

    CandidateContactController refactor it for html forms

--- src/main/java/com/board/job/controller/candidate/CandidateContactController.java
+++ src/main/java/com/board/job/controller/candidate/CandidateContactController.java
@@ -3,7 +3,9 @@ package com.board.job.controller.candidate;
 import com.board.job.model.dto.candidate_contact.CandidateContactRequest;
 import com.board.job.model.dto.candidate_contact.CandidateContactResponse;
 import com.board.job.model.mapper.candidate.CandidateContactMapper;
+import com.board.job.service.UserService;
 import com.board.job.service.candidate.CandidateContactService;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -11,7 +13,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.io.IOException;
 
 import static com.board.job.controller.AuthoritiesHelper.getAuthorities;
 
@@ -21,49 +27,64 @@ import static com.board.job.controller.AuthoritiesHelper.getAuthorities;
 @RequestMapping("/api/users/{owner-id}/candidate-contacts")
 public class CandidateContactController {
     private final CandidateContactMapper mapper;
+    private final UserService userService;
     private final CandidateContactService candidateContactService;
 
     @GetMapping("/{id}")
     @PreAuthorize("@authCandidateContactService." +
-            "isUserAdminOrUsersSameByIdAndUserOwnerCandidateContacts(#ownerId, #id, authentication.principal)")
-    public CandidateContactResponse getCandidateById(@PathVariable("owner-id") long ownerId, @PathVariable long id,
-                                                     Authentication authentication) {
-        var candidateContact = mapper.getCandidateContactResponseFromCandidateContact(candidateContactService.readById(id));
-        log.info("=== GET-CANDIDATE_CONTACT === {} == {}", getAuthorities(authentication), authentication.getPrincipal());
+            "isUserAdminOrUsersSameByIdAndUserOwnerCandidateContacts(#ownerId, #id, authentication.name)")
+    public ModelAndView getCandidateById(@PathVariable("owner-id") long ownerId, @PathVariable long id,
+                                         Authentication authentication, ModelMap map) {
+        log.info("=== GET-CANDIDATE_CONTACT === {} == {}", getAuthorities(authentication), authentication.getName());
+        map.addAttribute("owner", userService.readById(ownerId));
+        map.addAttribute("candidateContactRequest", mapper
+                .getCandidateContactRequestFromCandidateContact(candidateContactService.readById(id)));
 
-        return candidateContact;
+        return new ModelAndView("candidate-contact-get", map);
+    }
+
+    @GetMapping("/create")
+    @PreAuthorize("@userAuthService.isUserAdminOrUsersSameById(#ownerId, authentication.name)")
+    public ModelAndView createRequest(@PathVariable("owner-id") long ownerId,
+                                         Authentication authentication, ModelMap map) {
+        map.addAttribute("owner", userService.readById(ownerId));
+        map.addAttribute("candidateContactRequest", new CandidateContactRequest());
+
+        return new ModelAndView("candidate-contact-create", map);
     }
 
     @PostMapping
-    @PreAuthorize("@userAuthService.isUserAdminOrUsersSameById(#ownerId, authentication.principal)")
-    public ResponseEntity<String> create(@PathVariable("owner-id") long ownerId, @RequestBody @Valid CandidateContactRequest request, Authentication authentication) {
-        candidateContactService.create(ownerId, mapper.getCandidateContactFromCandidateContactRequest(request));
-        log.info("=== POST-CANDIDATE_CONTACT === {} == {}", getAuthorities(authentication), authentication.getPrincipal());
+    @ResponseStatus(HttpStatus.CREATED)
+    @PreAuthorize("@userAuthService.isUserAdminOrUsersSameById(#ownerId, authentication.name)")
+    public void create(@PathVariable("owner-id") long ownerId, @RequestBody @Valid CandidateContactRequest request,
+                       Authentication authentication, HttpServletResponse response) throws IOException {
+        var created = candidateContactService.create(ownerId, mapper.getCandidateContactFromCandidateContactRequest(request));
+        log.info("=== POST-CANDIDATE_CONTACT === {} == {}", getAuthorities(authentication), authentication.getName());
 
-        return ResponseEntity.status(HttpStatus.CREATED)
-                .body("Your contacts successfully recorded.");
+        response.sendRedirect(String.format("/api/users/%d/candidate-contacts/%d", ownerId, created.getId()));
     }
 
-    @PutMapping("/{id}")
+    @PostMapping("/{id}/update")
     @PreAuthorize("@authCandidateContactService." +
-            "isUsersSameByIdAndUserOwnerCandidateContacts(#ownerId, #id, authentication.principal)")
-    public ResponseEntity<String> update(@PathVariable("owner-id") long ownerId, @PathVariable long id,
-                                         @RequestBody @Valid CandidateContactRequest request, Authentication authentication) {
+            "isUsersSameByIdAndUserOwnerCandidateContacts(#ownerId, #id, authentication.name)")
+    public void update(@PathVariable("owner-id") long ownerId, @PathVariable long id, @Valid CandidateContactRequest request,
+                       Authentication authentication, HttpServletResponse response) throws Exception {
+
         candidateContactService.update(id, mapper.getCandidateContactFromCandidateContactRequest(request));
-        log.info("=== PUT-CANDIDATE_CONTACT === {} == {}", getAuthorities(authentication), authentication.getPrincipal());
+        log.info("=== PUT-CANDIDATE_CONTACT === {} == {}", getAuthorities(authentication), authentication.getName());
 
-        return ResponseEntity.ok("Your contacts successfully updated.");
+        response.sendRedirect(String.format("/api/users/%d/candidate-contacts/%d", ownerId, id));
     }
 
-    @DeleteMapping("/{id}")
+    @GetMapping("/{id}/delete")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     @PreAuthorize("@authCandidateContactService." +
-            "isUsersSameByIdAndUserOwnerCandidateContacts(#ownerId, #id, authentication.principal)")
-    public ResponseEntity<String> delete(@PathVariable("owner-id") long ownerId, @PathVariable long id,
-                                         Authentication authentication) {
-        var candidateContact = candidateContactService.readById(id);
+            "isUsersSameByIdAndUserOwnerCandidateContacts(#ownerId, #id, authentication.name)")
+    public void delete(@PathVariable("owner-id") long ownerId, @PathVariable long id,
+                                         Authentication authentication, HttpServletResponse response) throws IOException {
         candidateContactService.delete(id);
-        log.info("=== DELETE-CANDIDATE_CONTACT === {} == {}", getAuthorities(authentication), authentication.getPrincipal());
+        log.info("=== DELETE-CANDIDATE_CONTACT === {} == {}", getAuthorities(authentication), authentication.getName());
 
-        return ResponseEntity.ok(String.format("Candidate contact with name %s successfully deleted", candidateContact.getCandidateName()));
+        response.sendRedirect("/api/users/" + ownerId);
     }
 }
