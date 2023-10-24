b0335b1fb48dcaf4acb62a20d345f85d1b2cd368
maksym_korniev <maksimkarulet8@gmail.com>
2023-10-24 03:37:10 +0300

    WorkMode added new variable for WorkMode for better getting and understanding for users

--- src/main/java/com/board/job/model/entity/sample/WorkMode.java
+++ src/main/java/com/board/job/model/entity/sample/WorkMode.java
@@ -1,5 +1,24 @@
 package com.board.job.model.entity.sample;
 
+import lombok.Getter;
+
+@Getter
 public enum WorkMode {
-    CHOICE_OF_CANDIDATE, ONLY_OFFICE, ONLY_REMOTE, HYBRID
+    CHOICE_OF_CANDIDATE("Choice of Candidate"), ONLY_OFFICE("Only Office"),
+    ONLY_REMOTE("Only Remote"), HYBRID("Hybrid");
+
+    private String value;
+
+    WorkMode(String value) {
+        this.value = value;
+    }
+
+    public static WorkMode checkValuesAndGet(String insertedValue) {
+        for (WorkMode workMode : WorkMode.values()) {
+            if (insertedValue.equals(workMode.getValue())) {
+                return workMode;
+            }
+        }
+        throw new EnumConstantNotPresentException(LanguageLevel.class, "This work mode does not found!");
+    }
 }
