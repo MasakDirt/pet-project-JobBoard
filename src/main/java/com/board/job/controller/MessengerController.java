b52edc65d2905e7d207295f026499b387429ae2b
maksym_korniev <maksimkarulet8@gmail.com>
2023-10-25 00:41:30 +0300

    MessengerController refactor it for html form getting messengers

--- src/main/java/com/board/job/controller/MessengerController.java
+++ src/main/java/com/board/job/controller/MessengerController.java
@@ -1,7 +1,9 @@
 package com.board.job.controller;
 
 import com.board.job.model.dto.messenger.CutMessengerResponse;
+import com.board.job.model.entity.Messenger;
 import com.board.job.model.mapper.MessengerMapper;
+import com.board.job.service.FeedbackService;
 import com.board.job.service.MessengerService;
 import com.board.job.service.UserService;
 import lombok.AllArgsConstructor;
@@ -10,8 +12,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 import static com.board.job.controller.AuthoritiesHelper.getAuthorities;
@@ -24,33 +29,38 @@ public class MessengerController {
     private final UserService userService;
     private final MessengerMapper mapper;
     private final MessengerService messengerService;
+    private final FeedbackService feedbackService;
 
     @GetMapping("/candidate/{candidate-id}/messengers")
     @PreAuthorize("@authCandidateProfileService.isUserAdminOrUsersSameByIdAndUserOwnerCandidateProfile" +
-            "(#ownerId, #candidateId, authentication.principal)")
-    public List<CutMessengerResponse> getAllCandidateMessengers(@PathVariable("owner-id") long ownerId,
-                                                                @PathVariable("candidate-id") long candidateId, Authentication authentication) {
-        var responses = messengerService.getAllByCandidateId(candidateId)
+            "(#ownerId, #candidateProfileId, authentication.name)")
+    public ModelAndView getAllCandidateMessengers(@PathVariable("owner-id") long ownerId,
+                                                  @PathVariable("candidate-id") long candidateProfileId,
+                                                  Authentication authentication, ModelMap map) {
+        var messengers = messengerService.getAllByCandidateProfileId(candidateProfileId)
                 .stream()
-                .map(mapper::getCutMessengerResponseFromMessenger)
+                .map(messenger -> mapper.getCutMessengerResponseFromMessenger(messenger, feedbackService.getLastFeedbackText(messenger.getId())))
                 .toList();
-        log.info("=== GET-CANDIDATE-MESSENGERS === {} == {}", getAuthorities(authentication), authentication.getPrincipal());
+        log.info("=== GET-CANDIDATE-MESSENGERS === {} == {}", getAuthorities(authentication), authentication.getName());
+        map.addAttribute("owner", userService.readById(ownerId));
+        map.addAttribute("dateFormatter", DateTimeFormatter.ofPattern("dd MMM"));
+        map.addAttribute("messengers", messengers);
 
-        return responses;
+        return new ModelAndView("messengers-list", map);
     }
 
     @GetMapping("/employer-profile/{employer-id}/vacancies/{vacancy-id}/messengers")
     @PreAuthorize("@authVacancyService.isUsersSameAndEmployerProfileOwnerOfVacancy" +
-            "(#ownerId, #employerId, #vacancyId, authentication.principal)")
+            "(#ownerId, #employerId, #vacancyId, authentication.name)")
     public List<CutMessengerResponse> getAllVacancyMessengers(
             @PathVariable("owner-id") long ownerId, @PathVariable("employer-id") long employerId,
             @PathVariable("vacancy-id") long vacancyId, Authentication authentication
     ) {
         var responses = messengerService.getAllByVacancyId(vacancyId)
                 .stream()
-                .map(mapper::getCutMessengerResponseFromMessenger)
+                .map(messenger -> mapper.getCutMessengerResponseFromMessenger(messenger, feedbackService.getLastFeedbackText(messenger.getId())))
                 .toList();
-        log.info("=== GET-VACANCY-MESSENGERS === {} == {}", getAuthorities(authentication), authentication.getPrincipal());
+        log.info("=== GET-VACANCY-MESSENGERS === {} == {}", getAuthorities(authentication), authentication.getName());
 
         return responses;
     }
@@ -62,50 +72,50 @@ public class MessengerController {
             @PathVariable("owner-id") long ownerId, @PathVariable("employer-id") long employerId,
             @PathVariable("vacancy-id") long vacancyId, Authentication authentication
     ) {
-        var principal = authentication.getPrincipal();
+        var name = authentication.getName();
         var messenger = messengerService.create(vacancyId,
-                userService.readByEmail(principal.toString()).getCandidateProfile().getId());
-        log.info("=== POST-MESSENGER === {} == {}", getAuthorities(authentication), principal);
+                userService.readByEmail(name).getCandidateProfile().getId());
+        log.info("=== POST-MESSENGER === {} == {}", getAuthorities(authentication), name);
 
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(String.format("Messenger between %s and candidate %s successfully created",
                         messenger.getVacancy().getEmployerProfile().getCompanyName(),
-                        messenger.getCandidate().getOwner().getName()));
+                        messenger.getCandidateProfile().getOwner().getName()));
     }
 
     @DeleteMapping("/candidate/{candidate-id}/messengers/{id}")
     @PreAuthorize("@authMessengerService.isUsersSameByIdAndUserOwnerCandidateProfileAndCandidateProfileContainMessenger" +
-            "(#ownerId, #candidateId, #id, authentication.principal)")
+            "(#ownerId, #candidateId, #id, authentication.name)")
     public ResponseEntity<String> deleteByCandidate(
             @PathVariable("owner-id") long ownerId, @PathVariable("candidate-id") long candidateId,
             @PathVariable long id, Authentication authentication
     ) {
         var messenger = messengerService.readById(id);
         messengerService.delete(id);
-        log.info("=== DELETE-MESSENGER-BY-CANDIDATE === {} == {}", getAuthorities(authentication), authentication.getPrincipal());
+        log.info("=== DELETE-MESSENGER-BY-CANDIDATE === {} == {}", getAuthorities(authentication), authentication.getName());
 
         return ResponseEntity.ok(
                 String.format("Messenger between %s and candidate %s successfully deleted",
                         messenger.getVacancy().getEmployerProfile().getCompanyName(),
-                        messenger.getCandidate().getOwner().getName())
+                        messenger.getCandidateProfile().getOwner().getName())
         );
     }
 
     @DeleteMapping("/employer-profile/{employer-id}/vacancies/{vacancy-id}/messengers/{id}")
     @PreAuthorize("@authMessengerService.isUsersSameByIdAndUserOwnerEmployerProfileAndEmployerOwnerVacancyAndVacancyContainMessenger" +
-            "(#ownerId, #employerId, #vacancyId, #id, authentication.principal)")
+            "(#ownerId, #employerId, #vacancyId, #id, authentication.name)")
     public ResponseEntity<String> deleteByEmployer(
             @PathVariable("owner-id") long ownerId, @PathVariable("employer-id") long employerId,
             @PathVariable("vacancy-id") long vacancyId, @PathVariable long id, Authentication authentication
     ) {
         var messenger = messengerService.readById(id);
         messengerService.delete(id);
-        log.info("=== DELETE-MESSENGER-BY-EMPLOYER === {} == {}", getAuthorities(authentication), authentication.getPrincipal());
+        log.info("=== DELETE-MESSENGER-BY-EMPLOYER === {} == {}", getAuthorities(authentication), authentication.getName());
 
         return ResponseEntity.ok(
                 String.format("Messenger between %s and candidate %s successfully deleted",
                         messenger.getVacancy().getEmployerProfile().getCompanyName(),
-                        messenger.getCandidate().getOwner().getName())
+                        messenger.getCandidateProfile().getOwner().getName())
         );
     }
 }
