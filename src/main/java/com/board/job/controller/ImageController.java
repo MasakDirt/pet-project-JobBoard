616e78b180969d9ed025cd45e9b2b89d4e4d9da6
maksym_korniev <maksimkarulet8@gmail.com>
2023-10-27 21:31:19 +0300

    ImageController removed unused imports

--- src/main/java/com/board/job/controller/ImageController.java
+++ src/main/java/com/board/job/controller/ImageController.java
@@ -6,9 +6,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.io.ByteArrayResource;
-import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.Resource;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -18,7 +16,6 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 
 import static com.board.job.controller.AuthoritiesHelper.getAuthorities;
