888d5f8e95c694ae7df65608ef325e17eb87a1aa
maksym_korniev <maksimkarulet8@gmail.com>
2023-10-31 02:32:08 +0200

    MessengerRepository added new method for getting messenger by ownerID and vacancyId

--- src/main/java/com/board/job/repository/MessengerRepository.java
+++ src/main/java/com/board/job/repository/MessengerRepository.java
@@ -5,10 +5,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface MessengerRepository  extends JpaRepository<Messenger, Long> {
     List<Messenger> findAllByCandidateProfileId(long candidateId);
 
     List<Messenger> findAllByVacancyId(long vacancyId);
+
+    Optional<Messenger> findByCandidateProfile_OwnerIdAndVacancyId(long ownerId, long vacancyId);
 }
