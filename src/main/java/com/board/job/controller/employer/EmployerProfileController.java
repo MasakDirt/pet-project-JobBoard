eb462bd1011dd45f3a5d8de593060a911d04a729
maksym_korniev <maksimkarulet8@gmail.com>
2023-11-02 02:22:29 +0200

    EmployerProfileController refactor class for html forms

--- src/main/java/com/board/job/controller/employer/EmployerProfileController.java
+++ src/main/java/com/board/job/controller/employer/EmployerProfileController.java
@@ -1,77 +1,93 @@
 package com.board.job.controller.employer;
 
 import com.board.job.model.dto.employer_profile.EmployerProfileRequest;
-import com.board.job.model.dto.employer_profile.EmployerProfileResponse;
 import com.board.job.model.mapper.employer.EmployerProfileMapper;
+import com.board.job.service.UserService;
 import com.board.job.service.employer.EmployerProfileService;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.io.IOException;
 
 import static com.board.job.controller.AuthoritiesHelper.getAuthorities;
-import static org.springframework.http.ResponseEntity.*;
 
 @Slf4j
 @RestController
 @AllArgsConstructor
 @RequestMapping("/api/users/{owner-id}/employer-profiles")
 public class EmployerProfileController {
+    private final UserService userService;
     private final EmployerProfileMapper mapper;
     private final EmployerProfileService employerProfileService;
 
     @GetMapping("/{id}")
     @PreAuthorize("@authEmployerProfileService.isUserAdminOrUsersSameByIdAndUserOwnerEmployerProfile" +
             "(#ownerId, #id, authentication.name)")
-    public EmployerProfileResponse getById(@PathVariable("owner-id") long ownerId,
-                                           @PathVariable long id, Authentication authentication) {
+    public ModelAndView getById(@PathVariable("owner-id") long ownerId,
+                                @PathVariable long id, ModelMap map, Authentication authentication) {
 
         var employerProfile = mapper.getEmployerProfileResponseFromEmployerProfile(employerProfileService.readById(id));
+        map.addAttribute("owner", userService.readById(ownerId));
+        map.addAttribute("employerProfile", employerProfile);
         log.info("=== GET-EMPLOYER_PROFILE === {} == {}", getAuthorities(authentication), authentication.getName());
 
-        return employerProfile;
+        return new ModelAndView("employers/employer-profile-get", map);
+    }
+
+    @GetMapping("/{id}/create")
+    @PreAuthorize("@userAuthService.isUsersSame(#ownerId, authentication.name)")
+    public ModelAndView getCreateForm(@PathVariable("owner-id") long ownerId, ModelMap map) {
+
+        map.addAttribute("owner", userService.readById(ownerId));
+        map.addAttribute("employerProfileRequest", new EmployerProfileRequest());
+
+        return new ModelAndView("employers/employer-profile-create", map);
     }
 
     @PostMapping
-    @PreAuthorize("@userAuthService.isUsersSame(#ownerId, authentication.principal)")
-    public ResponseEntity<String> create(@PathVariable("owner-id") long ownerId,
-                                         @RequestBody @Valid EmployerProfileRequest request, Authentication authentication) {
+    @ResponseStatus(HttpStatus.CREATED)
+    @PreAuthorize("@userAuthService.isUsersSame(#ownerId, authentication.name)")
+    public void create(@PathVariable("owner-id") long ownerId,
+                       @Valid EmployerProfileRequest request, Authentication authentication,
+                       HttpServletResponse response) throws IOException {
 
         var created = employerProfileService.create(ownerId, mapper.getEmployerProfileFromEmployerProfileRequest(request));
-        log.info("=== POST-EMPLOYER_PROFILE === {} == {}", getAuthorities(authentication), authentication.getPrincipal());
+        log.info("=== POST-EMPLOYER_PROFILE === {} == {}", getAuthorities(authentication), authentication.getName());
 
-        return status(HttpStatus.CREATED).body(
-                String.format("Employer profile for user %s successfully created", created.getOwner().getName()));
+        response.sendRedirect(String.format("/api/users/%d/employer-profiles/%d", ownerId, created.getId()));
     }
 
-    @PutMapping("/{id}")
+    @PostMapping("/{id}/update")
     @PreAuthorize("@authEmployerProfileService.isUsersSameByIdAndUserOwnerEmployerProfile" +
-            "(#ownerId, #id, authentication.principal)")
-    public ResponseEntity<String> update(@PathVariable("owner-id") long ownerId, @PathVariable long id,
-                                         @RequestBody @Valid EmployerProfileRequest request, Authentication authentication) {
+            "(#ownerId, #id, authentication.name)")
+    public void update(@PathVariable("owner-id") long ownerId, @PathVariable long id,
+                       @Valid EmployerProfileRequest request, Authentication authentication,
+                       HttpServletResponse response) throws IOException {
 
         var updated = employerProfileService.update(id, mapper.getEmployerProfileFromEmployerProfileRequest(request));
-        log.info("=== PUT-EMPLOYER_PROFILE === {} == {}", getAuthorities(authentication), authentication.getPrincipal());
+        log.info("=== PUT-EMPLOYER_PROFILE === {} == {}", getAuthorities(authentication), authentication.getName());
 
-        return ok(
-                String.format("Employer profile for user %s successfully updated", updated.getOwner().getName()));
+        response.sendRedirect(String.format("/api/users/%d/employer-profiles/%d", ownerId, id));
     }
 
-    @DeleteMapping("/{id}")
+    @GetMapping("/{id}/delete")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     @PreAuthorize("@authEmployerProfileService.isUsersSameByIdAndUserOwnerEmployerProfile" +
-            "(#ownerId, #id, authentication.principal)")
-    public ResponseEntity<String> delete(@PathVariable("owner-id") long ownerId, @PathVariable long id,
-                                         Authentication authentication) {
+            "(#ownerId, #id, authentication.name)")
+    public void delete(@PathVariable("owner-id") long ownerId, @PathVariable long id,
+                       Authentication authentication, HttpServletResponse response) throws IOException {
 
-        var employerProfile = employerProfileService.readById(id);
         employerProfileService.delete(id);
-        log.info("=== DELETE-EMPLOYER_PROFILE === {} == {}", getAuthorities(authentication), authentication.getPrincipal());
+        log.info("=== DELETE-EMPLOYER_PROFILE === {} == {}", getAuthorities(authentication), authentication.getName());
 
-        return ok(
-                String.format("Employer profile for user %s successfully deleted", employerProfile.getOwner().getName()));
+        response.sendRedirect(String.format("/api/users/%d/employer-profiles/%d", ownerId, id));
     }
 }
