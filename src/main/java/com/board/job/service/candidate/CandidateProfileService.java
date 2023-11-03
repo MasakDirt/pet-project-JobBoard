518c8f45de16ac6f62f5a8a4b29ae2d375d2b79c
maksym_korniev <maksimkarulet8@gmail.com>
2023-11-03 02:50:26 +0200

    CandidateProfileService created new method findBySearchText and refactor getAllSorted with new methods

--- src/main/java/com/board/job/service/candidate/CandidateProfileService.java
+++ src/main/java/com/board/job/service/candidate/CandidateProfileService.java
@@ -1,15 +1,19 @@
 package com.board.job.service.candidate;
 
+import com.board.job.controller.HelperForPagesCollections;
 import com.board.job.model.entity.candidate.CandidateProfile;
 import com.board.job.repository.candidate.CandidateProfileRepository;
 import com.board.job.service.UserService;
+import com.board.job.service.VacancyService;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.springframework.data.domain.Sort.*;
 
@@ -44,11 +48,20 @@ public class CandidateProfileService {
         candidateProfileRepository.delete(readById(id));
     }
 
-    public List<CandidateProfile> getAll() {
-        return candidateProfileRepository.findAll(sort(CandidateProfile.class));
+    public Page<CandidateProfile> getAllSorted(Pageable pageable, String searchText) {
+        if (HelperForPagesCollections.checkSearchText(searchText)){
+            return findBySearchText(pageable, searchText);
+        }
+        return candidateProfileRepository.findAll(pageable);
     }
 
-    public Page<CandidateProfile> getAllSorted(Pageable pageable) {
-        return candidateProfileRepository.findAll(pageable);
+    private Page<CandidateProfile> findBySearchText(Pageable pageable, String searchText) {
+        return candidateProfileRepository.findAll()
+                .stream()
+                .filter(candidateProfile -> candidateProfile.getPosition().contains(searchText))
+                .collect(Collectors.collectingAndThen(
+                        Collectors.toList(),
+                        list -> new PageImpl<>(list, pageable, pageable.getPageSize())
+                ));
     }
 }
