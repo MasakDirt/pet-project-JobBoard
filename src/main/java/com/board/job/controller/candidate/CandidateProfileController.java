4f375c69cf3df47b490186288958fdc54eb54a59
maksym_korniev <maksimkarulet8@gmail.com>
2023-11-03 02:47:29 +0200

    CandidateProfileController refactor method getSortedCandidates for html form

--- src/main/java/com/board/job/controller/candidate/CandidateProfileController.java
+++ src/main/java/com/board/job/controller/candidate/CandidateProfileController.java
@@ -1,7 +1,6 @@
 package com.board.job.controller.candidate;
 
 import com.board.job.model.dto.candidate_profile.CandidateProfileRequest;
-import com.board.job.model.dto.candidate_profile.CutCandidateProfileResponse;
 import com.board.job.model.entity.sample.Category;
 import com.board.job.model.entity.sample.LanguageLevel;
 import com.board.job.model.mapper.candidate.CandidateProfileMapper;
@@ -12,7 +11,6 @@ import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -23,11 +21,9 @@ import org.springframework.web.servlet.ModelAndView;
 
 import java.io.IOException;
 import java.util.Arrays;
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 import static com.board.job.controller.AuthoritiesHelper.getAuthorities;
+import static com.board.job.controller.HelperForPagesCollections.*;
 
 @Slf4j
 @RestController
@@ -38,23 +34,28 @@ public class CandidateProfileController {
     private final UserService userService;
     private final CandidateProfileService candidateProfileService;
 
-    @GetMapping()
+    @GetMapping
     @PreAuthorize("hasAnyRole('ADMIN', 'EMPLOYER')")
-    public List<CutCandidateProfileResponse> getSortedCandidates(
+    public ModelAndView getSortedCandidates(
             @RequestParam(name = "sort_by", defaultValue = "id") String[] sortBy,
-            @RequestParam(name = "sort_order", defaultValue = "desc") String sortedOrder,
-            @RequestParam(defaultValue = "0") int page, Authentication authentication) {
+            @RequestParam(name = "sort_order", defaultValue = "asc") String sortedOrder,
+            @RequestParam(name = "page", required = false, defaultValue = "0") int page,
+            @RequestParam(name = "searchText", required = false, defaultValue = "") String searchText,
+            Authentication authentication, ModelMap map) {
 
-        var sort = Sort.by(Sort.Direction.fromString(sortedOrder), sortBy);
-        Pageable pageable = PageRequest.of(page, 5, sort);
-
-        var candidateProfiles = candidateProfileService.getAllSorted(pageable)
-                .stream()
-                .map(mapper::getCutCandidateProfileResponseFromCandidateProfile)
-                .toList();
+        map.addAttribute("page", page);
+        map.addAttribute("searchText", searchText);
+        map.addAttribute("sort_order", sortedOrder);
+        map.addAttribute("sort_by", getSortByValues(Arrays.toString(sortBy)));
+        map.addAttribute("owner", userService.readByEmail(authentication.getName()));
+        map.addAttribute("candidates", candidateProfileService.getAllSorted(
+                        PageRequest.of(page, 5, Sort.by(Sort.Direction.fromString(sortedOrder), sortBy)),
+                        searchText
+                )
+        );
         log.info("=== GET-SORTED-CANDIDATE_PROFILES === {} == {}", getAuthorities(authentication), authentication.getName());
 
-        return candidateProfiles;
+        return new ModelAndView("employers/candidates-list", map);
     }
 
     @GetMapping("/{id}")
@@ -77,7 +78,7 @@ public class CandidateProfileController {
     @PreAuthorize("@userAuthService.isUserAdminOrUsersSameById(#ownerId, authentication.name)")
     public ModelAndView createRequest(@PathVariable("owner-id") long ownerId, ModelMap map) {
         map.addAttribute("owner", userService.readById(ownerId));
-        map.addAttribute("candidateProfileRequest",new CandidateProfileRequest());
+        map.addAttribute("candidateProfileRequest", new CandidateProfileRequest());
         map.addAttribute("categories", Arrays.stream(Category.values()).map(Category::getValue));
         map.addAttribute("eng_levels", Arrays.stream(LanguageLevel.values()).map(LanguageLevel::getValue));
         map.addAttribute("ukr_levels", Arrays.stream(LanguageLevel.values()).map(LanguageLevel::getValue));
@@ -85,11 +86,11 @@ public class CandidateProfileController {
         return new ModelAndView("candidates/candidate-profile-create", map);
     }
 
-    @PostMapping()
+    @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     @PreAuthorize("@userAuthService.isUserAdminOrUsersSameById(#ownerId, authentication.name)")
     public void create(@PathVariable("owner-id") long ownerId, @Valid CandidateProfileRequest request,
-            Authentication authentication, HttpServletResponse response) throws IOException {
+                       Authentication authentication, HttpServletResponse response) throws IOException {
 
         candidateProfileService.create(ownerId, mapper.getCandidateProfileFromCandidateProfileRequest(request));
         log.info("=== POST-CANDIDATE_PROFILE === {} == {}", getAuthorities(authentication), authentication.getName());
